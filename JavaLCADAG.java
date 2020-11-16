d04682481de4d43ed2734dc47a2d54329586e8e3
andymcdonald99 <mcdonaa6@tcd.ie>
2020-11-16 13:53:45 +0000

    Added BinaryTree
    Added the binary tree from lca last assignment with modifications for DAG

--- JavaLCADAG.java
+++ JavaLCADAG.java
@@ -10,5 +10,32 @@ public class JavaLCADAG {
 			this.childLeft = childLeft;
 			this.childRight = childRight;
 		}
+	
+	}
+
+	public class binaryTree {
+		treeNode head;
+
+		binaryTree() {
+			head = null;
+		}
+
+		public int getKey(treeNode theNode) {
+			if (theNode != null)
+				return theNode.data;
+			return -1;
+		}
+
+		public void add(int data, treeNode parent, boolean isLeft) {
+			treeNode newNode = new treeNode(data, parent, null, null);
+			if (head == null) { 
+				head = newNode;
+				return;
+			}
+			if (isLeft)
+				parent.childLeft = newNode;
+			else
+				parent.childRight = newNode;
+		}
 	}
 }
